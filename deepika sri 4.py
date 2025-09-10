650d334c419dcf2577063978b45a63e15bb24718
deepi30112007 <deepi30112007@gmail.com>
2025-09-10 17:46:18 +0530

    Add files via upload

---
+++ deepika sri 4.py
@@ -0,0 +1,10 @@
+d={}
+n=int(input("enter the number of key value pairs"))
+for i in range(n):
+    key=input("enter the key :")
+    value=int(input("enter the value:"))
+    d[key]=value
+print("the dictionary is:",d)
+    
+   
+    
