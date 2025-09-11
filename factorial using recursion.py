61111162ccb975a0fee0de1c4a186c2866e32016
deepi30112007 <deepi30112007@gmail.com>
2025-09-11 18:17:01 +0530

    Add files via upload

---
+++ factorial using recursion.py
@@ -0,0 +1,3 @@
+def greet(name):
+    print("welcome to coding:",name)
+greet("deepika sri")
---
+++ function 1.py
@@ -0,0 +1,10 @@
+def area(length,breadth):
+    return length*breadth
+print("area of rectangle:",area(10,20))
+print("area of rectangle:",area(5,15))
+
+
+def add(a,b):
+    return a+b
+c=add(3,5)
+print(c)
