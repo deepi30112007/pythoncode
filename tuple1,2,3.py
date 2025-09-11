c9eb73aa4b43cfdb3f7e5465f09bbfde58eeeb4c
deepi30112007 <deepi30112007@gmail.com>
2025-09-11 17:41:56 +0530

    Add files via upload

---
+++ tuple1,2,3.py
@@ -0,0 +1,18 @@
+tup=(5,'welcome',7,'greeks')
+print(tup)
+
+
+
+
+tup1=(0,1,2,3)
+tup2=(4,6,7)
+tup3=("deepika sri")
+print(tup3)
+
+
+
+b=(1,2,3,4,5,6,7)
+print(b[1:])
+print(b[::-1])
+print(b[::4])
+print(b[2:])
