806ea27deebef7504b932f8c4dcdc5dbc00029e2
deepi30112007 <deepi30112007@gmail.com>
2025-09-16 14:22:54 +0530

    Add files via upload

---
+++ string 07.py
@@ -0,0 +1,16 @@
+s="python"
+new_s="j"+s[1:]
+print(new_s)
+
+
+
+
+s="hello world"
+updated=s[:6]+"python"
+print(updated)
+
+
+
+name="deepika sri"
+age=17
+print(f"my name is deepika sri and i am {age} years old")
