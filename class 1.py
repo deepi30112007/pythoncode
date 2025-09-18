eb214fff78fbac7375176da2aedd619d4a5e27a7
deepi30112007 <deepi30112007@gmail.com>
2025-09-18 17:30:15 +0530

    Add files via upload

---
+++ class 1.py
@@ -0,0 +1,11 @@
+class stud:
+       def__init__(self,a,b,c)
+          self.sno=a
+          self.name=b
+          self.name=c
+       def disp(self):
+          print(self.sno)
+          print(self.name)
+          print(self.age)
+ob=stud(123,'abc',18)
+ob.disp()
